46704965b19eaddd13b01d13d463778b96397a5a
Shawn Analla <shawnanalla@gmail.com>
2021-12-23 15:47:24 -0800

    updated readme, added catch for invalid inputs

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# random-password-generator
+# <Random Password Generator>
+
+## Description
+This is a simple password generator that collects inputs from the user regarding criteria they'd like applied to their password, such as password length and whether or not they'd like to include special symbols. Coming up with semi-safe passwords is now as simple as a couple clicks of a button! Gone are the days where one must frustratingly repeat the password-creating process once they realize the password they created doesn't have enough capital letters. The text generated can be copied directly from the browser and applied wherever needed. This project taught me a lot about how JS functions work and how I can harness user inputs for generating content. 
+
+## Installation
+The site for the password generator is live at:
+[https://shnala.github.io/random-password-generator/]
+
+The repository for this site is linked here:
+[https://github.com/shnala/random-password-generator]
+
+## Usage
+The generator works via a series of prompts for the user which allow them to specify what criteria they'd like to apply to their password, giving them some control over the "randomness" element. They will be told to specify a length for the password between 8 and 128 characters (the generator will not create a password if an inappropriate value is input). They will then recieve a short series of confirm or exit boxes which determine if their password will include such elements as upper and lowercase letters, numbers, and special symbols (if none of these criteria are accepted, a password will not be generated). The generator will then pool all of their selected criteria and draw from it a random string which appears directly on the page. It can then be copied and pasted wherever needed. The generator can be used repeatedly.
+
+Screenshots of the generator in action:
+![Prompt for length](/assets/generatorprompt1.PNG?raw=true "Optional Title")
+![Prompt for uppercase](/assets/generatorprompt2.PNG?raw=true "Optional Title")
+![Final product](/assets/generatorprompt3.PNG)
+
+Provide instructions and examples for use. Include screenshots as needed.
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+## Credits
+This README.md was created with reference to the following site: [https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide]
--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // Assigns variable "generateBtn" to the button in index.html. Defining character strings for user input to draw from.
 var generateBtn = document.querySelector("#generate");
-// const lowercase = "abcdefghijklmnopqrstuvwxyz";
-// const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// const numbers = "0123456789";
-// const symbols = "!@#$%^&*_-+=";
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -13,11 +9,19 @@ var result = '';
 
 
 // Write password to the #password input
-function generatePassword(passwordLength, inclLowercase, inclUppercase, inclNumbers, inclSpecial) {
+function generatePassword() {
 
 //Prompts for desired criteria
-//TODO: specify length to be b/t 8 and 128 (if statement after prompt; call again if incorrect)
   var passwordLength = prompt("Please specify the desired length for your password, between 8-128.")
+
+    if (passwordLength < 8) {
+      alert("Value must be between 8 and 128.")
+      return;
+
+    } else if (passwordLength > 128) {
+      alert("Value must be between 8 and 128.")
+      return;
+    }
 
   var inclLowercase = confirm("Press 'OK' if you'd like your password to include lowercase letters.")
 
@@ -28,43 +32,38 @@ function generatePassword(passwordLength, inclLowercase, inclUppercase, inclNumb
   var inclSpecial = confirm("Press 'OK' if you'd like your password to include special characters.")  
 
 //Concatenating desired criteria into one array  
+
     if (inclLowercase === true) {
       desiredInputs = desiredInputs.concat(lowercase);
+      //legacy code for self-reference
       // for (let i = 0; i < lowercase.length; i++) {
       //   result += lowercase.charAt(Math.floor(Math.random() * lowercase.length));
       // }
     }
-    console.log(desiredInputs)
 
     if (inclUppercase === true) {
       desiredInputs = desiredInputs.concat(uppercase);
-      // for (let i = 0; i < uppercase.length; i++) {
-      //   result += uppercase.charAt(Math.floor(Math.random() * uppercase.length));
-      // }
+      
     } 
-    console.log(desiredInputs)
 
     if (inclNumbers === true) {
       desiredInputs = desiredInputs.concat(numbers);
-      // for (let i = 0; i < numbers.length; i++) {
-      //   result += numbers.charAt(Math.floor(Math.random() * numbers.length));
-      // }
+      
     } 
 
     if (inclSpecial === true) {
       desiredInputs = desiredInputs.concat(symbols);
-      // for (let i = 0; i < symbols.length; i++) {
-      //   result += symbols.charAt(Math.floor(Math.random() * symbols.length));
-      // }
+      
     } 
 
-    console.log(desiredInputs);
-  // return result;
+    if (!inclLowercase && !inclUppercase && !inclNumbers && !inclSpecial) {
+      alert("At least one criteria must be selected.")
+    }
 
-  //declare pw array
+  //Declare pw array
   var shuffledArray = []
 
-  //write loop for pw length
+  //write loop for pw length, thanks Brett
 for (let i = 0; i < passwordLength; i++) {
   //generate
     var randomIndex = Math.floor(Math.random() * desiredInputs.length);
@@ -73,26 +72,13 @@ for (let i = 0; i < passwordLength; i++) {
     shuffledArray.push(shuffledChar);
 }
 
+//Push that into a pw array
+//Convert array into string
   var pwOutput = shuffledArray.join('');
 
 return pwOutput;
 
-//declare pw array
-//write loop that runs for pw length
-//each iteration generate a random index;
-//use that random index to select an item from the desiredInputs array
-//push that into into a pw array
-//convert array to string
-//return pw string
-
-
 }
-
-
-//TODO: create array from user's desired inputs
-//TODO: shuffle that array
-//TODO: slice that array to the length specified by the user
-
 
 function writePassword() {
   var password = generatePassword();
@@ -104,21 +90,3 @@ function writePassword() {
 
 // Add event listener to generate button. This will call the "writePassword" function.
 generateBtn.addEventListener("click", writePassword);
-
-// // document.getElementById("generate").addEventListener("click", function(){
-
-//   }
-
-// )
-
-
-
-//collect user choice
-  //prompt for length (8 to 128 characters)
-  //prompt for including lowercase
-  //prompt for including uppercase
-  //prompt for including numerals
-  //prompt for including special characters
-//validate at least one choice has been made
-//pull from active criteria
-
